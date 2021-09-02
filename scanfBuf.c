07b028e9d265639032bb2507a8bd3c32f58bd254
Choiyu330 <jenny4063@naver.com>
2021-09-02 21:27:23 +0900

    add 1Week file

---
+++ scanfBuf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int main(void)
+{
+	char string[20];
+	char c;
+	
+	scanf("%s",string);
+	scanf("%c",&c);
+	
+	printf("%s\n",string);
+	printf("!!%c!!\n",c);
+}
+
